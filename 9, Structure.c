daf1aeb4b5e6352d26e996baf734872f05f5892f
Shrikanta Paul <122515906+shrikanta71@users.noreply.github.com>
2023-01-25 13:19:06 +0600

    Update 9, Structure.c

--- 9, Structure.c
+++ 9, Structure.c
@@ -6,6 +6,12 @@ struct Student{
     int age;
 };
 
+struct Car{
+    char name[50];
+    int year;
+    char place[50];
+};
+
 int main()
 {
   struct Student Student1 = {"Shrikanta Paul", "221-15-4868", 21};
@@ -16,6 +22,19 @@ int main()
   printf("%s %s %d\n", Student2.name, Student2.Id, Student2.age);
   printf("%s %s %d\n", Student3.name, Student3.Id, Student3.age);
 
+
+  // Take input in structure.
+  struct Car car1;
+  int n, i; // n represents the number of time we want to take a car brand details.
+
+  scanf("%d", &n);
+
+  for(i=0; i<n; i++)
+  {
+      scanf("%s %d %s", car1.name, &car1.year, car1.place);
+
+      printf("%s\t%d\t%s\n", car1.name, car1.year, car1.place);
+  }
+
   return 0;
 }
-
